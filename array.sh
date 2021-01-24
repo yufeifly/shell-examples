04191119bb2d38c1f7da904827781e16bb38bc27
yufeifly <yufei.xiong@qq.com>
2021-01-24 22:05:31 +0800

    first commit

---
+++ array.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#array_name=("value0", "value1", "value2", "value3")
+#
+#echo "fuck you: ${array_name[0]}"
+#echo $$
+#
+##your_name="qinjx"
+##echo $your_name
+##echo ${your_name}
+
+
+my_array=(A B "C" D)
+
+echo "第一个元素为: ${my_array[0]}"
+echo "第二个元素为: ${my_array[1]}"
+echo "第三个元素为: ${my_array[2]}"
+echo "第四个元素为: ${my_array[3]}"
